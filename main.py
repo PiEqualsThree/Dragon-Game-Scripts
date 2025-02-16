40c6041829a39e50449fdf615952daa6a8c458db
Hyperbit FN <hyerbitfn@gmail.com>
2025-02-16 16:46:10 -0600

    FInally done ??

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import math
 import csv
+import json
 
 baseColumnIdx = 0
 nameColumnIdx = 1
@@ -8,6 +9,7 @@ rarityColumnIdx = 2
 dropWeightColumnIdx = 3
 powerColumnIdx = 4
 expressionColumnIdx = 5
+oddsColumnIdx = 6
 
 totalCommonWeight = 0
 totalUncommonWeight = 0
@@ -16,12 +18,12 @@ totalEpicWeight = 0
 totalLegendaryWeight = 0
 
 rarityWeights = {
-    "Common" : 5000000,
-    "Uncommon" : 1000000,
-    "Rare" : 25000,
+    "Common" : 7500000,
+    "Uncommon" : 150000,
+    "Rare" : 15000,
     "Epic" : 2000,
-    "Legendary" : 10,
-    "Mythic" : 1
+    "Legendary" : 50,
+    "Mythic" : 2
 }
 
 rarityOddsDenominators = {
@@ -33,35 +35,51 @@ rarityOddsDenominators = {
     "Mythic" : 1
 }
 
-petRarityTotalWeights = {
-    "Common" : 1,
-    "Uncommon" : 1,
-    "Rare" : 1,
-    "Epic" : 1,
-    "Legendary" : 1,
-    "Mythic" : 1
+petOdds = {
+    "Common" :    { "totalWeight" : 0, "petOdds": {}},
+    "Uncommon" :  { "totalWeight" : 0, "petOdds": {}},
+    "Rare" :      { "totalWeight" : 0, "petOdds": {}},
+    "Epic" :      { "totalWeight" : 0, "petOdds": {}},
+    "Legendary" : { "totalWeight" : 0, "petOdds": {}},
+    "Mythic" :    { "totalWeight" : 0, "petOdds": {}},
 }
 
-commonPetOddsDeonimators = {}
-uncommonPetOddsDeonimators = {}
-rarePetOddsDeonimators = {}
-epicPetOddsDeonimators = {}
-LegendaryPetOddsDeonimators = {}
-MythicPetOddsDeonimators = {}
-
-
 def main():
-    with open('DragonPetData.csv', 'r', encoding='utf-8') as file:
+    with open('PetData.csv', 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         title = next(reader)
+        rows = list(reader)
+        print(rows)
+        modifyRowDropWeights(rows)
         calculateRarityOdds()
+        calculatePetOdds(rows)
 
-        # with open('PetData.csv', 'w', newline="") as newFile:
-        #     writer = csv.writer(newFile)
-        #     writer.writerow(title)
-        #     for row in reader:
-        #         modifyRowDropWeight(row)
-        #         writer.writerow(row)  
+        with open('new_pet_data.csv', 'w', newline="") as newFile:
+            writer = csv.writer(newFile)
+            writer.writerow(title)
+            for row in rows:
+                print("Row stuff")
+                petRarityOdds = rarityOddsDenominators[row[rarityColumnIdx]]
+                petIndividualOdds = petOdds[row[rarityColumnIdx]]["petOdds"][row[nameColumnIdx]]
+                petTotalOdds = normalizeDropOdds(petRarityOdds * petIndividualOdds)
+                row[oddsColumnIdx] = petTotalOdds
+                writer.writerow(row)  
+
+
+def calculatePetOdds(rows):
+    # Calculate totals 
+    for row in rows:
+        petOdds[row[rarityColumnIdx]]["totalWeight"] += int(row[dropWeightColumnIdx])
+
+    # Calculate pet odds
+    for row in rows:
+        petName = row[nameColumnIdx]
+        petDropOdds = math.floor(petOdds[row[rarityColumnIdx]]["totalWeight"] / int(row[dropWeightColumnIdx]))
+        petDropOdds = petDropOdds
+        petOdds[row[rarityColumnIdx]]["petOdds"][petName] = petDropOdds
+
+    print(petOdds)
+
 
 def calculateRarityOdds():
     totalWeight = 0
@@ -74,26 +92,41 @@ def calculateRarityOdds():
         rarityOddsDenominators[rarity] = odds
         print(f"Rarity {rarity} has 1 in {odds} odds!")        
 
-def calculatePetOdds(petrows, rarity):
+def modifyRowDropWeights(rows):
+    for row in rows:
+        if row[rarityColumnIdx] == "Common":
+            row[dropWeightColumnIdx] = str(random.randint(random.randint(1, 9), random.randint(10, 15)))
+
+        elif row[rarityColumnIdx] == "Uncommon":
+            row[dropWeightColumnIdx] = str(random.randint(random.randint(1, 15), random.randint(20, 40)))
+
+        elif row[rarityColumnIdx] == "Rare":
+            row[dropWeightColumnIdx] = str(random.randint(random.randint(1, 20), random.randint(25, 100)))
+
+        elif row[rarityColumnIdx] == "Epic":
+            row[dropWeightColumnIdx] = str(random.randint(random.randint(1, 30), random.randint(35, 300)))
+
+        elif row[rarityColumnIdx] == "Legendary":
+            row[dropWeightColumnIdx] = str(random.randint(random.randint(1, 45), random.randint(55, 750)))
+
+        elif row[rarityColumnIdx] == "Mythic":
+            row[dropWeightColumnIdx] = str(random.randint(random.randint(0, 80), random.randint(150, 1000)))
+
+def normalizeDropOdds(dropOdd):
+    if dropOdd > 19:
+        tenths = math.floor(math.log10(dropOdd))
+        roundFactor = max(5 * math.pow(10, tenths - 2), 5)
+        normalizedValue = roundFactor * math.floor(dropOdd / roundFactor)
+        return normalizedValue
+    else:
+        return dropOdd
+
+
+def test():
     pass
 
-def modifyRowDropWeight(row):
-    if row[rarityColumnIdx] == "Common":
-        row[dropWeightColumnIdx] = str(random.randint(40, 100))
+def formatInt(Number):
+    if Number < 1000: return Number
 
-    elif row[rarityColumnIdx] == "Uncommon":
-        row[dropWeightColumnIdx] = str(random.randint(15, 110))
-
-    elif row[rarityColumnIdx] == "Rare":
-        row[dropWeightColumnIdx] = str(random.randint(10, 150))
-
-    elif row[rarityColumnIdx] == "Epic":
-        row[dropWeightColumnIdx] = str(random.randint(10, 200))
-
-    elif row[rarityColumnIdx] == "Legendary":
-        row[dropWeightColumnIdx] = str(random.randint(1, 300))
-
-    elif row[rarityColumnIdx] == "Mythic":
-        row[dropWeightColumnIdx] = str(random.randint(1, 1000))
 
 main()
---
+++ pet_data_to_verse.py
@@ -0,0 +1,54 @@
+import csv
+
+baseColumnIdx = 0
+nameColumnIdx = 1
+rarityColumnIdx = 2
+dropWeightColumnIdx = 3
+powerColumnIdx = 4
+expressionColumnIdx = 5
+oddsColumnIdx = 6
+
+def main():
+    with open('PetData.csv', 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)
+        rows = list(reader)
+        with open('pet_data.txt', 'w') as txtFile:
+            for row in rows:
+                faceDirectory = row[baseColumnIdx][:-2]
+                txtFile.write(f"{row[nameColumnIdx]}=>pet_data:\n")
+                txtFile.write(f"    Power := {row[powerColumnIdx]}\n")
+                txtFile.write(f"    PetName := {row[nameColumnIdx]}\n")
+                txtFile.write(f"    PetMesh := PetAssets.{row[baseColumnIdx][:-2]}.{row[baseColumnIdx]}.{row[baseColumnIdx]}\n")
+                txtFile.write(f"    FaceTexture := PetAssets.{row[baseColumnIdx][:-2]}.{row[baseColumnIdx].lower()}_face\n")
+                txtFile.write(f"    ExpressionTexture := PetAssets.Expressions.{row[expressionColumnIdx]}\n")
+                txtFile.write(f"    RarityData := pet_rarity_data:\n        RarityTier := pet_rarity.{row[rarityColumnIdx]}\n        RarityWeight := {row[dropWeightColumnIdx]}\n        RarityString := \"1 in {row[oddsColumnIdx]}\"\n\n")
+                
+
+
+
+main()
+
+
+
+# "Prism"=>pet_data:
+#         Power := 8
+#         PetName := "Prism"
+#         PetMesh := PetAssets.Diamond.Diamond_1.Diamond_1
+#         FaceTexture := PetAssets.Diamond.diamond_1_face
+#         ExpressionTexture := PetAssets.Expressions.Angry
+#         RarityData := pet_rarity_data:
+#             RarityTier := pet_rarity.Rare
+#             RarityWeight := 100
+
+
+
+# "Terra"=>pet_data:
+#     Power := 5
+#     PetName := "Terra"
+#     PetMesh := PetAssets.Earth.Earth_1.Earth_1
+#     FaceTexture := PetAssets.Earth.earth_1_face
+#     ExpressionTexture := PetAssets.Expressions.Unibrow
+#     RarityData := pet_rarity_data:
+#         RarityTier := pet_rarity.Common
+#         RarityWeight := 150
